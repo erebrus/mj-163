b53e7c327ce81e3973f3fac9d29f04167518e8ec
Claudio Coelho <claudio.coelho@solenix.ch>
2024-07-20 10:54:35 +0200

    scoring

--- godot/src/common/types.gd
+++ godot/src/common/types.gd
@@ -14,3 +14,11 @@ enum CakeTags {
 	NonGluten,
 	Vegan,
 }
+
+const ScoreTable := {
+	ChildState.CRYING : 50,
+	ChildState.ABOUT_TO_CRY : 200,
+	ChildState.UPSET : 500,
+	ChildState.NORMAL : 1000,
+	
+}
