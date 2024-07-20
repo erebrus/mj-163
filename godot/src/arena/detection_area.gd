0f45d6ceae0fa8a085cc75937cdd119a8d952014
Claudio Coelho <claudio.coelho@solenix.ch>
2024-07-20 10:19:37 +0200

    children state

--- godot/src/arena/detection_area.gd
+++ godot/src/arena/detection_area.gd
@@ -2,12 +2,12 @@ extends Area2D
 class_name DetectionArea
 
 const SIZE := Vector2(960,250)
-const MARGIN := 100
+const MARGIN := 50
 var direction := Types.Direction.LEFT
 
 	
 func get_spawn_point()->Vector2:
-	return global_position + Vector2((SIZE.x + MARGIN) * direction,0)
+	return global_position + Vector2((SIZE.x/2 + MARGIN) * direction,0)
 
 func get_body_count()->int:
 	return get_overlapping_bodies().size()
